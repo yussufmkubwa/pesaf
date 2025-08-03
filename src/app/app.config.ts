d42e0086c55599bd740773cac003e3c6f83334e6
alhajjmuhammed <alhajjmuhammed@gmail.com>
2025-08-02 21:52:04 -0700

    Fix SSR injector error by removing client hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
@@ -9,7 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes), 
-    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
     provideAnimationsAsync()
   ]
